6d3d8c7f779f20c3f5028ac1c332281806e247c1
Sam O'Cain <samocain93@gmail.com>
2023-04-26 21:02:06 -0500

    server basic structure and post model created. initial routes created

--- server/index.js
+++ server/index.js
@@ -3,13 +3,25 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
 
-app.use(bodyParser.json({ limit: "30mb", extended: true }))
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+app.use('/posts', postRoutes)
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
 // connect do mongo db
 
-const CONNECTION_URL = 
+const CONNECTION_URL =
+  'mongodb+srv://sam1234:sam1234@cluster0.rsfrllb.mongodb.net/test';
+const PORT = process.env.PORT || 5000;
 
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  )
+  .catch((error) => console.log(error));
